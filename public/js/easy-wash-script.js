d796cad6e67a11a43155d3fd912d51911883223e
ayn2110 <reder777@gmail.com>
2022-07-01 13:27:40 +0800

    0.4.0(2022-07-1) easy wash

--- public/js/easy-wash-script.js
+++ public/js/easy-wash-script.js
@@ -4,9 +4,9 @@
 // 定義視窗寬度的變數
 let windowWidth = $(window).width()
 
+// 進網頁直接執行一次 判斷
 if ( windowWidth >= 768 ){
     // 大於等於768執行
-
     // $('nav>ul>li').hover(function(){}, function(){})
     $('nav>ul>li').hover(function(){
         // 滑鼠滑入執行
@@ -16,7 +16,6 @@ if ( windowWidth >= 768 ){
         $(this).find('.submenu').removeClass('active')
 
     })
-
 }else{
     // 小於768執行
     $('#btn-nav-switch').on('click', function(){
@@ -26,6 +25,32 @@ if ( windowWidth >= 768 ){
     $('nav>ul>li').on('click',function(){
         $(this).find('.submenu').toggleClass('active')
     })// nav>ul>li end
-}//
+}// windowWidth>=768 end
 
+// 每一次縮放視窗的時候執行
+$(window).resize(function(){
+    // widthWidth 在每次縮放視窗寬度時，換上當前的視窗寬度的數值
+    windowWidth = $(window).width()
+    if ( windowWidth >= 768 ){
+        // 大於等於768執行
+        // $('nav>ul>li').hover(function(){}, function(){})
+        $('nav>ul>li').hover(function(){
+            // 滑鼠滑入執行
+            $(this).find('.submenu').addClass('active')
+        }, function(){
+            // 滑鼠滑出執行
+            $(this).find('.submenu').removeClass('active')
+    
+        })
+    }else{
+        // 小於768執行
+        $('#btn-nav-switch').on('click', function(){
+            $('nav').toggleClass('active')
+        })// btn-nav-switch end
+        
+        $('nav>ul>li').on('click',function(){
+            $(this).find('.submenu').toggleClass('active')
+        })// nav>ul>li end
+    }// windowWidth>=768 end
+})// window resize end
 
